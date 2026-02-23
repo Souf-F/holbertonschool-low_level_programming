1c6f37499f9c3aec45b689fc3e7e84ff8377ca75
Souf-F <sfecom.31000@gmail.com>
2026-02-23 11:44:54 +0100

    fix: remove unnecessary blank lines and ensure newline at end of file in 4-puts.c

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
-
 /**
  * main - Point d'entrée du programme
  *
  * Description: Affiche une phrase spécifique en utilisant la fonction puts
  * Return: Toujours 0 (Succès)
  */
-
 int main(void)
 {
     puts("\"Programming is like building a multilingual puzzle");
