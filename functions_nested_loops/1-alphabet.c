ff8af83e6853cfdd8a63820420e1816d8d6ee53e
Souf-F <sfecom.31000@gmail.com>
2026-02-24 10:34:16 +0100

    feat: create print alphabet with void fonction

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_alphabet - imprime l'alphabet en minuscules
+ *
+ * Return: void (ne renvoie rien)
+ */
+void print_alphabet(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
