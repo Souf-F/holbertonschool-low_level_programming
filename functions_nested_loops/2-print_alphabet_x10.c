56eaa32444c82c2e993f686ab47c90a08769e4b6
Souf-F <sfecom.31000@gmail.com>
2026-02-24 14:35:05 +0100

    feat: basma zouaoui

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_alphabet_x10 - imprime 10 fois l'alphabet en minuscules
+ *
+ * Return: void
+ */
+void print_alphabet_x10(void)
+{
+int i;
+char lettre;
+
+
+for (i = 0; i < 10; i++)
+{
+lettre = 'a';
+
+while (lettre <= 'z')
+{
+_putchar(lettre);
+lettre++;
+}
+putchar('\n');
+}
+}
