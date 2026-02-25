99a9bff6a74f4b488c47ba06208902790e734fa4
Souf-F <sfecom.31000@gmail.com>
2026-02-25 14:19:54 +0100

    feat :

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include "main.h"
+
+/**
+ * _isupper - checks for uppercase character
+ * @c: the character to check
+ *
+ * Return: 1 if c is upercase, 0 otherwise
+ */
+int _isupper(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
