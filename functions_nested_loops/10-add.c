9f6873209e33bcd3781ab258c4384d8a0cb6dfa4
Souf-F <sfecom.31000@gmail.com>
2026-02-25 08:10:47 +0100

    feat: add function for aditions

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * add - adds two integers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: the sum of a and b
+ */
+int add(int a, int b)
+{
+return (a + b);
+}
