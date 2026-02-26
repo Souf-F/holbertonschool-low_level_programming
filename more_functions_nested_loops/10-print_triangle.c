1706d663dfcb56e5d3446ce9b6e3db368e322982
Souf-F <sfecom.31000@gmail.com>
2026-02-26 10:50:55 +0100

    feat : add script to creat a pyramid

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_triangle - Prints a triangle, followed by a new line.
+ * @size: The size of the triangle.
+ *
+ * Return: void.
+ */
+void print_triangle(int size)
+{
+	int i, j;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 0; i < size; i++)
+		{
+			for (j = 0; j <= i; j++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+}
