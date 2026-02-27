163d68b6b228476f78c19305c8c3f60b94ab6a9b
Souf-F <sfecom.31000@gmail.com>
2026-02-27 13:47:05 +0100

    feat : Implement addition end-to-end.

--- simple-calculator/calculator.c
+++ simple-calculator/calculator.c
@@ -9,7 +9,7 @@ int main(void)
 {
 	int choixMenu;
 
-	printf("Simple Calculator");
+	printf("Simple Calculator\n");
 	printf("1 Add\n");
 	printf("2 Soustraction\n");
 	printf("3 Multiplication\n");
@@ -18,14 +18,49 @@ int main(void)
 
 	scanf("%d", &choixMenu);
 
-	if (choixMenu == 0)
+	if  (choixMenu == 0)
 	{
 		printf("Bye !\n");
 
-	} else
+	}
+	else if (choixMenu == 1)
+{
+	int a, b, resultat;
+
+printf("A:");
+	scanf("%d", &a);
+printf("B:");
+	scanf("%d", &b);
+		resultat = a + b;
+
+	printf("Resultat: %d\n", resultat);
+}
+
+else
 	{
 	printf("Invalid Choice\n");
 	}
-
 	return (0);
+
+}
+/**
+ * Addition - Calcule la somme de deux entiers
+ * @a: Le premier entier
+ * @b: Le second entier
+ *
+ * Return: La somme de a et b
+ */
+int Addition(int a, int b)
+{
+	int resultat;
+
+	printf("a");
+	scanf("%d", &a);
+	printf("b");
+	scanf("%d", &b);
+
+	resultat = a + b;
+	printf("Resultat %d\n", resultat);
+
+	return (resultat);
 }
