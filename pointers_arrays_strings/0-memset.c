3fcae3d765170a88f4c23ec341d140edcffa5498
Souf-F <sfecom.31000@gmail.com>
2026-03-05 12:57:29 +0100

    fix: error about betty

--- pointers_arrays_strings/0-memset.c
+++ pointers_arrays_strings/0-memset.c
@@ -1,5 +1,13 @@
 #include "main.h"
 
+/**
+ * _memset - Fills memory with a constant byte.
+ * @s: The memory area to be filled.
+ * @b: The constant byte to fill the memory with.
+ * @n: The number of bytes to be filled.
+ *
+ * Return: A pointer to the memory area s.
+ */
 char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i;
@@ -8,5 +16,6 @@ char *_memset(char *s, char b, unsigned int n)
 	{
 		s[i] = b;
 	}
+
 	return (s);
 }
