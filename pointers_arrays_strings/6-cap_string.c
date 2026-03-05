8872733f107e60e2f856ce8874927bd6444654db
Souf-F <sfecom.31000@gmail.com>
2026-03-05 09:01:38 +0100

    feat: add a script for cap_string and fix: puts 2

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -1,14 +1,57 @@
 #include "main.h"
 /**
- * _memcpy - Copies n bytes from memory area src to memory area dest.
- * @dest: Pointer to the destination memory area.
- * @src: Pointer to the source memory area.
- * @n: Number of bytes to copy.
- * Return: Pointer to the destination memory area (dest).
+ * grand_separateur - Checks if a character is a word separator.
+ * @c: The character to check.
+ *
+ * Return: 1 if it is a separator, 0 otherwise.
+ */
+int grand_separateur(char c)
+{
+	if (c == ' ' || c == '\t' || c == '\n' || c == ','
+		|| c == ';' || c == '.' || c == '!' || c == '?'
+		|| c == '"' || c == '(' || c == ')' || c == '{'
+		|| c == '}')
+		return (1);
+	return (0);
+}
+/**
+ * cap_string - Capitalizes all words of a string.
+ * @s: The string to be modified.
+ *
+ * Description: Capitalizes the first letter of every word based on
+ * specific separators and ensures other letters are lowercase.
+ *
+ * Return: Pointer to the modified string s.
  */
 char *cap_string(char *s)
 {
-	int i;
+	int i = 0;
+	int separateur = 1;
 
-	while( i = 0 )
+	while (s[i] != '\0')
+	{
+		if (grand_separateur(s[i]))
+		{
+			separateur = 1;
+		}
+		else if (s[i] >= 'a' && s[i] <= 'z' && separateur == 1)
+		{
+			s[i] -= 32;
+			separateur = 0;
+		}
+		else if (s[i] >= 'A' && s[i] <= 'Z' && separateur == 0)
+		{
+			s[i] += 32;
+		}
+		else if (s[i] >= 'A' && s[i] <= 'Z' && separateur == 1)
+		{
+			separateur = 0;
+		}
+		else
+		{
+			separateur = 0;
+		}
+		i++;
+	}
+	return (s);
 }
