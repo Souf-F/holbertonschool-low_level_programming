53888c1ec6101e8140f887e63c8f5a1ee352e4ee
Souf-F <sfecom.31000@gmail.com>
2026-03-05 13:32:09 +0100

    feat: create script to call argv

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the name of the program.
+ * @argc: The number of arguments (unused).
+ * @argv: An array of strings containing the arguments.
+ *
+ * Return: Always 0 (Success).
+ */
+int main(int argc, char **argv)
+{
+	(void)argc;
+
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
