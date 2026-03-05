17b9da3cd8b4c7dd23921559d2833b7288d58a94
Souf-F <sfecom.31000@gmail.com>
2026-03-05 12:49:12 +0100

    Fix : betty issue

--- pointers_arrays_strings/5-string_toupper.c
+++ pointers_arrays_strings/5-string_toupper.c
@@ -1,22 +1,22 @@
 #include "main.h"
+
 /**
- * _memcpy - Copies n bytes from memory area src to memory area dest.
- * @dest: Pointer to the destination memory area.
- * @src: Pointer to the source memory area.
- * @n: Number of bytes to copy.
- * Return: Pointer to the destination memory area (dest).
+ * string_toupper - Changes all lowercase letters of a string to uppercase.
+ * @s: The string to be modified.
+ *
+ * Return: A pointer to the modified string.
  */
 char *string_toupper(char *s)
 {
-    int i = 0;
+	int i = 0;
 
-    while (s[i] != '\0')
-    {
-        if (s[i] >= 'a' && s[i] <= 'z')
-        {
-            s[i] = s[i] - 32;
-        }
-        i++;
-    }
-    return (s);
+	while (s[i] != '\0')
+	{
+		if (s[i] >= 'a' && s[i] <= 'z')
+		{
+			s[i] = s[i] - 32;
+		}
+		i++;
+	}
+	return (s);
 }
