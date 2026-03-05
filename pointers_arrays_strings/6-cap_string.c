863acf178647f93a71bbc220cf4d6eb9d942a44b
Souf-F <sfecom.31000@gmail.com>
2026-03-05 12:50:15 +0100

    fix error about betty

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * grand_separateur - Checks if a character is a word separator.
  * @c: The character to check.
@@ -7,19 +8,19 @@
  */
 int grand_separateur(char c)
 {
-	if (c == ' ' || c == '\t' || c == '\n' || c == ','
-		|| c == ';' || c == '.' || c == '!' || c == '?'
-		|| c == '"' || c == '(' || c == ')' || c == '{'
-		|| c == '}')
+	if (c == ' ' || c == '\t' || c == '\n' || c == ',' ||
+	    c == ';' || c == '.' || c == '!' || c == '?' ||
+	    c == '"' || c == '(' || c == ')' || c == '{' ||
+	    c == '}')
+	{
 		return (1);
+	}
 	return (0);
 }
+
 /**
  * cap_string - Capitalizes all words of a string.
  * @s: The string to be modified.
- *
- * Description: Capitalizes the first letter of every word based on
- * specific separators and ensures other letters are lowercase.
  *
  * Return: Pointer to the modified string s.
  */
@@ -42,9 +43,6 @@ char *cap_string(char *s)
 		else if (s[i] >= 'A' && s[i] <= 'Z' && separateur == 0)
 		{
 			s[i] += 32;
-		}
-		else if (s[i] >= 'A' && s[i] <= 'Z' && separateur == 1)
-		{
 			separateur = 0;
 		}
 		else
