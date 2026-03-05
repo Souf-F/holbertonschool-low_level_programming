c672cd3db10abbd0091f4fe63d35d99cd95b33a2
Souf-F <sfecom.31000@gmail.com>
2026-03-05 16:06:52 +0100

    fix:

--- argc_argv/1-args.c
+++ argc_argv/1-args.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
-#include <stdlib.h>
+
 /**
- * main - Affiche le nom du programme.
- * @argc: Le nombre d'arguments (non utilisé ici).
- * @argv: Le tableau des arguments de la ligne de commande.
+ * main - Prints the number of arguments passed into it.
+ * @argc: The number of command line arguments.
+ * @argv: An array containing the program command line arguments.
  *
- * Return: Toujours 0 (Succès).
+ * Return: Always 0 (Success).
  */
 int main(int argc, char *argv[])
 {
-	(void)argc;
-	printf("%s\n", argv[0]);
+	(void)argv;
+	printf("%d\n", argc - 1);
+
 	return (0);
 }
