d2de706fff757bf5255cd0a8800652effca1e8f8
Souf-F <sfecom.31000@gmail.com>
2026-03-05 12:42:27 +0100

    fix all error about betty

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -1,14 +1,17 @@
 #include "main.h"
-#include <stdlib.h>
+
 /**
- * @src: The source string to be copied.
- * @n: The maximum number of characters to copy from src.
- * Return: A pointer to the destination string (dest).
- * If src is n characters or more, dest will not be null-terminated.
+ * _strncpy - Copies a string
+ * @dest: The destination buffer
+ * @src: The source string to be copied
+ * @n: The maximum number of characters to copy from src
+ *
+ * Return: A pointer to the destination string (dest)
  */
 char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
+
 	for (i = 0; i < n && src[i] != '\0'; i++)
 	{
 		dest[i] = src[i];
@@ -17,5 +20,6 @@ char *_strncpy(char *dest, char *src, int n)
 	{
 		dest[i] = '\0';
 	}
+
 	return (dest);
 }
