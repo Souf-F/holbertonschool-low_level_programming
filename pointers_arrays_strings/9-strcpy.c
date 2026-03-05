d18718059d1f775ffd7f70b20e0610965b565a36
Souf-F <sfecom.31000@gmail.com>
2026-03-05 09:30:27 +0100

    fix : betty error

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -9,15 +9,15 @@
  */
 char *_strcpy(char *dest, char *src)
 {
-    char *ptr = dest;
+char *ptr = dest;
 
-    while (*src != '\0')
-    {
-        *ptr = *src;
-        ptr++;
-        src++;
-    }
-    *ptr = '\0';
+while (*src != '\0')
+{
+*ptr = *src;
+ptr++;
+src++;
+}
+*ptr = '\0';
 
-    return (dest);
+return (dest);
 }
