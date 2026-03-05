ca8c0a8807a889f5914f334bac26bf5fcef84b69
Souf-F <sfecom.31000@gmail.com>
2026-03-05 14:58:39 +0100

    add new script

---
+++ argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Affiche le nom du programme.
+ * @argc: Le nombre d'arguments (non utilisé ici).
+ * @argv: Le tableau des arguments de la ligne de commande.
+ *
+ * Return: Toujours 0 (Succès).
+ */
+int main(int argc, char *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
