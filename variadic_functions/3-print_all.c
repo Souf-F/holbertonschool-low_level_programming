9d93d1e3da8b98f1c87211bbb9af8cf2ca81cf17
Souf-F <sfecom.31000@gmail.com>
2026-03-15 16:04:48 +0100

    fix: error to cheker

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -1,33 +1,46 @@
 #include <stdio.h>
 #include <stdarg.h>
-
+/**
+ * print_all - prints anything based on a format string
+ * @format: list of types of arguments passed to the function
+ *          c: char, i: integer, f: float, s: char *
+ */
 void print_all(const char * const format, ...)
 {
 	va_list args;
 	const char *p;
 	char *s;
+	char *types;
 	int first;
+	int i;
 
 	va_start(args, format);
 	p = format;
+	types = "cifs";
 	first = 1;
 	while (p && *p)
 	{
+		i = 0;
 		s = NULL;
-		if (*p == 'c')
-			printf("%c", va_arg(args, int));
-		if (*p == 'i')
-			printf("%d", va_arg(args, int));
-		if (*p == 'f')
-			printf("%f", va_arg(args, double));
-		if (*p == 's')
+		while (types[i] && types[i] != *p)
+			i++;
+		if (types[i])
 		{
-			s = va_arg(args, char *);
-			printf("%s", s ? s : "(nil)");
+			if (!first)
+				printf(", ");
+			first = 0;
+			if (i == 0)
+				printf("%c", va_arg(args, int));
+			if (i == 1)
+				printf("%d", va_arg(args, int));
+			if (i == 2)
+				printf("%f", va_arg(args, double));
+			if (i == 3)
+			{
+				s = va_arg(args, char *);
+				printf("%s", s ? s : "(nil)");
+			}
 		}
-		if (!first && (*p == 'c' || *p == 'i' || *p == 'f' || *p == 's'))
-			printf(", ");
-		first = 0;
 		p++;
 	}
 	printf("\n");
