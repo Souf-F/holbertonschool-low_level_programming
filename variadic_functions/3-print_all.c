fef1d558381bc18e5ffcd1f92193a33fea487772
Souf-F <sfecom.31000@gmail.com>
2026-03-15 16:13:22 +0100

    fix: try to fix error to cheker

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -1,12 +1,42 @@
 #include "variadic_functions.h"
 #include <stdarg.h>
-#include <stdio.h>
+/**
+ * print_int - prints an integer using _putchar
+ * @n: the integer to print
+ */
+void print_int(int n)
+{
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+	if (n / 10)
+		print_int(n / 10);
+	_putchar((n % 10) + '0');
+}
+
+/**
+ * print_str - prints a string using _putchar
+ * @s: the string to print
+ */
+void print_str(char *s)
+{
+	int i;
+
+	i = 0;
+	if (!s)
+		s = "(nil)";
+	while (s[i])
+	{
+		_putchar(s[i]);
+		i++;
+	}
+}
 
 /**
  * print_all - prints anything based on a format string
  * @format: list of types of arguments passed to the function
- *
- * Return: void
  */
 void print_all(const char * const format, ...)
 {
@@ -20,25 +50,29 @@ void print_all(const char * const format, ...)
 	first = 1;
 	while (p && *p)
 	{
+		s = NULL;
 		if (*p == 'c' || *p == 'i' || *p == 'f' || *p == 's')
 		{
 			if (!first)
-				printf(", ");
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
 			first = 0;
 			if (*p == 'c')
-				printf("%c", va_arg(args, int));
+				_putchar(va_arg(args, int));
 			if (*p == 'i')
-				printf("%d", va_arg(args, int));
+				print_int(va_arg(args, int));
 			if (*p == 'f')
-				printf("%f", va_arg(args, double));
+				print_int((int)va_arg(args, double));
 			if (*p == 's')
 			{
 				s = va_arg(args, char *);
-				printf("%s", s ? s : "(nil)");
+				print_str(s);
 			}
 		}
 		p++;
 	}
-	printf("\n");
+	_putchar('\n');
 	va_end(args);
 }
