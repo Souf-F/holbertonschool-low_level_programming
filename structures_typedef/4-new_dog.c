27530a0de9803558755d5884cf12028ced7b3553
Souf-F <sfecom.31000@gmail.com>
2026-03-15 16:22:48 +0100

    fix:

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,38 +1,48 @@
-#include <stdlib.h>
-#include <string.h>
 #include "dog.h"
-
+#include <stdlib.h>
 /**
- * new_dog - Creates a new dog with copies of name and owner
- * @name: The name of the dog
- * @age: The age of the dog
- * @owner: The owner of the dog
+ * new_dog - Creates a new dog.
+ * @name: Name of the dog.
+ * @age: Age of the dog.
+ * @owner: Owner of the dog.
  *
- * Return: Pointer to the new dog_t structure, or NULL if it fails
+ * Return: Pointer to the new dog (dog_t), or NULL if it fails.
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *dog;
+	dog_t *d;
+	int i;
+	char *name_copy;
+	char *owner_copy;
 
-	dog = malloc(sizeof(dog_t));
-	if (dog == NULL)
+	d = malloc(sizeof(dog_t));
+	if (d == NULL)
 		return (NULL);
-
-	dog->name = strdup(name);
-	if (dog->name == NULL)
+	for (i = 0; name[i] != '\0'; i++)
+		;
+	name_copy = malloc(i + 1);
+	if (name_copy == NULL)
 	{
-		free(dog);
+		free(d);
 		return (NULL);
 	}
-
-	dog->owner = strdup(owner);
-	if (dog->owner == NULL)
+	for (i = 0; owner[i] != '\0'; i++)
+		;
+	owner_copy = malloc(i + 1);
+	if (owner_copy == NULL)
 	{
-		free(dog->name);
-		free(dog);
+		free(d);
+		free(name_copy);
 		return (NULL);
 	}
-
-	dog->age = age;
-	return (dog);
+	for (i = 0; name[i] != '\0'; i++)
+		name_copy[i] = name[i];
+	name_copy[i] = '\0';
+	for (i = 0; owner[i] != '\0'; i++)
+		owner_copy[i] = owner[i];
+	owner_copy[i] = '\0';
+	d->name = name_copy;
+	d->age = age;
+	d->owner = owner_copy;
+	return (d);
 }
